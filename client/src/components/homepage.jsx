48e688416b415fcf60512522ca6c7aec1b283469
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-03-07 12:19:26 +0100

    Improved Structure Of React App

---
+++ client/src/components/homepage.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class homepage extends Component {
+  state = {};
+  render() {
+    return <h4> Home </h4>;
+  }
+}
+
+export default homepage;
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import HomepageComponent from "../components/homepage";
+class Homepage extends Component {
+  state = {};
+  render() {
+    return <HomepageComponent />;
+  }
+}
+
+export default Homepage;
