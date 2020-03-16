6436ef562d8a6dc0ca22cad62acb6e68b4a85f08
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-03-16 09:48:00 +0100

    configjs download

--- scripts/git.js
+++ scripts/git.js
@@ -5,9 +5,9 @@ const commitMessage = args.join(" ");
 if(args.length === 0){console.log("Missing Commit Message"); return}
 
 exec(`git add . && git commit -m "${commitMessage}"`,(err,a)=>{
-	if(err){throw new Error("Error")};
+	if(err){console.log(err)};
 	exec("git push origin master",(err,a)=>{
-		if(err){throw new Error("Error")};
+		if(err){console.log(err)};
 		console.log(`Pushed Commit: ${commitMessage}`);
 		})
 })
