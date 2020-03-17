0cd8e056ae4a4b36fa0f1fd0e08eab2263b1ba97
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-03-17 16:27:07 +0100

    react is ts ready

--- scripts/git.js
+++ scripts/git.js
@@ -1,13 +1,42 @@
-const { exec} = require("child_process");
+const { exec } = require("child_process");
 const args = process.argv;
-args.splice(0,2);
+args.splice(0, 2);
 const commitMessage = args.join(" ");
-if(args.length === 0){console.log("Missing Commit Message"); return}
+if (args.length === 0) {
+  console.log("Missing Commit Message");
+  return;
+}
 
-exec(`git add . && git commit -m "${commitMessage}"`,(err,a)=>{
-	if(err){console.log(err);return};
-	exec("git push origin master",(err,a)=>{
-		if(err){console.log(err);return};
-		console.log(`Pushed Commit: ${commitMessage}`);
-		})
-})
+exec("git pull origin master", (err, a) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  let t = 1;
+  exec(`git add . && git commit -m "${commitMessage}"`, (err, a) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    t++;
+    exec("git push origin master", (err, a) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      t++;
+    });
+  });
+  switch (t) {
+    case 3:
+      console.log(`Pushed Commit: ${commitMessage}`);
+      break;
+    case 2:
+      console.log("Added & Comitted Commit.");
+      break;
+    case 1:
+      console.log("Pulled Potential Changes From Repo.");
+    default:
+      break;
+  }
+});
