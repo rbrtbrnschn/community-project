9ea77727f9b7c0043892ba31ace69f3919351889
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-03-27 12:10:57 +0100

    ADDED gulp sass, gulp watch-sass

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -5,8 +5,19 @@ gulp.task("hello",function(){
 	console.log("Hello World!");
 })
 
+// Compile All Sass (.scss not .sass - sorry,#2dumb4that) 
+// Pipes To src/css
+// Creates unwanted directories in src/css instead of just the files
 gulp.task("sass",function(){
 	return gulp.src("src/components/**/*.scss")
 	.pipe(sass())
 	.pipe(gulp.dest("src/css"))
 })
+
+// Watches ["sass"]
+gulp.task("watch-sass",function(){
+	gulp.watch("src/components/**/*.+(scss|sass)",gulp.series(["sass"]));
+})
+
+
+
