c0f5382e8c96b17d22e88f329e505050ad474969
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-03-28 10:13:58 +0100

    setup Navbar Component

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,10 +1,50 @@
 import React from "react";
+import {BasicNavItem, DropdownNavItem} from "./item"
+import Hamburger from "./hamburger"
+import "../../css/navbar/navbar.css";
 const Navbar = (props) => {
-	
+	/**
+	 * @BasicNavItem takes in: title,link
+	 * @param {title} Name Of The Link
+	 * @param {link} Href When Clicked On
+	 *
+	 * @DropdownNavItem takes in: title, pos, children
+	 * @param {title} Name Of The Dropdown Menu
+	 * @param {pos} Number [1,2,3], x>3 || x<1 wont work on mobile
+	 *@param {childrne} Array Of @BasicNavItem s
+	 */
+	const homepage = {
+		title:"Home",
+		link:"/"
+	}
+	const account = {
+		title: "Account",
+		pos:1,
+		children:[
+			{title: "Login",link:"/login"},
+			{title: "Register",link:"/createAccount"},
+			{title: "Logout",link:"/api/auth/logout"},
+		]
+	}
+
 	return(
-	<nav className="navbar is-danger">
+	<nav className="navbar is-danger" role="navigation" aria-label="main navigation">
+		<input type="checkbox" id="toggler" class="toggler" />
+		<div className="navbar-brand">
+			<BasicNavItem item={homepage} />
+			<Hamburger />
+		</div>
+		<div id="navbarBasicExample" className="navbar-menu">
+			<div className="navbar-start">
 		
-		</nav>
+			</div>
+			<div className="navbar-end">
+			  
+			  <DropdownNavItem item={account} />	
+			
+			</div>
+		</div>
+	</nav>
 	);
 }
 
---
+++ client/src/components/navbar/item.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const BasicNavItem = (props) =>{
+	const { item } = props;
+	const { title, link } = item;
+	const classes = "navbar-item";
+	return(
+		<a className={classes} href={link || "#"}>{title}</a>
+	);
+}
+
+
+const DropdownNavItem = (props) => {
+	const { item } = props;
+	const { title,pos,children } = item;
+	const classes = "navbar-item has-dropdown is-hoverable";
+	
+	return(
+		<div className={classes}>
+			<input type="checkbox" id={"dropdown"+pos} />
+			<label for={"dropdown"+pos} className="navbar-link">
+				{title}
+			</label>
+			<div id={"dropdown-content"+pos} className="navbar-dropdown">
+				{children.map((c,i)=>(<BasicNavItem item={c} />))}
+			</div>
+			
+
+		</div>
+	);
+}
+
+export default BasicNavItem;
+export { BasicNavItem, DropdownNavItem };
