86356790630e1581c9e2e5f5ce06c501b7b93df1
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-04-23 21:25:59 +0200

    changed brand item color to is-link

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,81 +1,96 @@
-import React, {useEffect, useState } from "react";
-import {BasicNavItem, DropdownNavItem} from "./item";
+import React, { useEffect, useState } from "react";
+import { BasicNavItem, DropdownNavItem } from "./item";
 import Hamburger from "./hamburger";
 import "../../css/navbar/navbar.css";
 import { config } from "../../config";
 const { uri } = config;
-const Navbar = props => {
-	const [logged,setLogged] = useState(true)
+const Navbar = (props) => {
+  const [logged, setLogged] = useState(true);
 
-	useEffect(()=>{
-	  fetch(uri.domain+"/api/auth")
-		.then(res=>res.json())
-		.then(user=>{
-		if(user.ok === false){
-		setLogged(false)
-		}
-		else{
-		setLogged(true)
-		}
-		})
-	})
-	/**
-	 * @BasicNavItem takes in: title,link
-	 * @param {title} Name Of The Link
-	 * @param {link} Href When Clicked On
-	 *
-	 * @DropdownNavItem takes in: title, pos, children
-	 * @param {title} Name Of The Dropdown Menu
-	 * @param {pos} Number [1,2,3], x>3 || x<1 wont work on mobile
-	 *@param {childrne} Array Of @BasicNavItem s
-	 */
-	const tasks = {
-		title:"Tasks",
-		link:"/tasks"
-	}
-	const profile = {
-		title:"Profile",
-		link:"/profile"
-	}
-	const friends = {
-		title:"Friends",
-		link:"/friends"
-	}
-	const account = {
-		title: "Account",
-		pos:1,
-		children:[
-			{title: "Profile",link:"/profile"},
-			{title: "Logout",link:`${uri.domain}/api/auth/logout`},
-		]
-	}
+  useEffect(() => {
+    fetch(uri.domain + "/api/auth")
+      .then((res) => res.json())
+      .then((user) => {
+        if (user.ok === false) {
+          setLogged(false);
+        } else {
+          setLogged(true);
+        }
+      });
+  });
+  /**
+   * @BasicNavItem takes in: title,link
+   * @param {title} Name Of The Link
+   * @param {link} Href When Clicked On
+   *
+   * @DropdownNavItem takes in: title, pos, children
+   * @param {title} Name Of The Dropdown Menu
+   * @param {pos} Number [1,2,3], x>3 || x<1 wont work on mobile
+   *@param {childrne} Array Of @BasicNavItem s
+   */
+  const tasks = {
+    title: "Tasks",
+    link: "/tasks",
+  };
+  const profile = {
+    title: "Profile",
+    link: "/profile",
+  };
+  const friends = {
+    title: "Friends",
+    link: "/friends",
+  };
+  const account = {
+    title: "Account",
+    pos: 1,
+    children: [
+      { title: "Profile", link: "/profile" },
+      { title: "Logout", link: `${uri.domain}/api/auth/logout` },
+    ],
+  };
 
-	return(
-	<nav className="navbar has-shadow is-spaced is-fixed-top" role="navigation" aria-label="main navigation">
-		<div className="container">
-		<input type="checkbox" id="toggler" className="toggler" />
-		<div className="navbar-brand">
-			<a className="navbar-item" style={{fontFamily:"aliseo"}} href="/tasks">TodoHub</a>
-			<Hamburger />
-		</div>
-		<div id="navbarBasicExample" className="navbar-menu">
-			<div className="navbar-start">
-			<BasicNavItem item={tasks} />
-			<BasicNavItem item={profile} />
-			<BasicNavItem item={friends} />
-			</div>
-			<div className="navbar-end">
-			{logged ?
-			  <DropdownNavItem item={account} /> :
-			  <button className="button" onClick={()=>{
-			    window.location = "/login"
-			  }}>Login</button>
-			}	
-			</div>
-		</div>
-	</div>
-	</nav>
-	);
-}
+  return (
+    <nav
+      className="navbar has-shadow is-spaced is-fixed-top"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <input type="checkbox" id="toggler" className="toggler" />
+        <div className="navbar-brand">
+          <a
+            className="navbar-item"
+            style={{ fontFamily: "aliseo", color: "#3273dc" }}
+            href="/tasks"
+          >
+            TodoHub
+          </a>
+          <Hamburger />
+        </div>
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <BasicNavItem item={tasks} />
+            <BasicNavItem item={profile} />
+            <BasicNavItem item={friends} />
+          </div>
+          <div className="navbar-end">
+            {logged ? (
+              <DropdownNavItem item={account} />
+            ) : (
+              <button
+                className="button"
+                onClick={() => {
+                  window.location = "/login";
+                }}
+              >
+                Login
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
